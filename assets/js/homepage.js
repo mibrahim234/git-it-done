13856e0311ef8475bccf2c0b47815ab2cbef3b2e
mibrahim234 <mohamm1417@gmail.com>
2021-09-15 15:47:20 -0400

    fixed up buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -44,6 +44,20 @@ var getUserRepos = function(user) {
     });
 };
 
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert('Error: GitHub User Not Found');
+    }
+  });
+};
+
 var displayRepos = function(repos, searchTerm) {
   // check if api returned any repos
   if (repos.length === 0) {
